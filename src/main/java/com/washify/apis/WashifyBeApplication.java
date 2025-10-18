a093256a5f3538701fd0dc33ce0e2171a50af448
BachTo <144211430+AnyaChann@users.noreply.github.com>
2025-10-18 23:00:21 +0700

    Prj Root

---
+++ src/main/java/com/washify/apis/WashifyBeApplication.java
@@ -0,0 +1,13 @@
+package com.washify.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashifyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WashifyBeApplication.class, args);
+    }
+
+}
